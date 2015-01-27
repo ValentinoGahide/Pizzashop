c47c0bd4bfe281783d0da72ca3e9b218cbf0e37a
ValentinoGahide <valentinogahide@gmail.com>
2015-01-27 15:18:14 +0100

    test

--- data/DBConfig.php
+++ data/DBConfig.php
@@ -2,8 +2,8 @@
 
 namespace data;
 
-if (!isset($DB)) {
-    $DB = new DBconfig();
+if (!isset($dbh)) {
+    $dbh = new DBconfig();
 }
 
 class DBconfig {
@@ -11,21 +11,28 @@ class DBconfig {
     public static $DB_CONNSTRING = null;
     public static $DB_USERNAME = null;
     public static $DB_PASSWORD = null;
+    public $dbh; // handle of the db connexion
 
     function __construct() {
-        define('DB_HOST', getenv('OPENSHIFT_MYSQL_DB_HOST'));
-        define('DB_PORT', getenv('OPENSHIFT_MYSQL_DB_PORT'));
-        define('DB_USER', getenv('OPENSHIFT_MYSQL_DB_USERNAME'));
-        define('DB_PASS', getenv('OPENSHIFT_MYSQL_DB_PASSWORD'));
-        define('DB_NAME', getenv('OPENSHIFT_APP_NAME'));
+        $host = getenv("OPENSHIFT_MYSQL_DB_HOST");
+        $port = getenv('OPENSHIFT_MYSQL_DB_PORT');
+        DBconfig::$DB_USERNAME = getenv('OPENSHIFT_MYSQL_DB_USERNAME');
+        DBconfig::$DB_PASSWORD = getenv('OPENSHIFT_MYSQL_DB_PASSWORD');
+        DBconfig::$DB_CONNSTRING = "mysql:host=$host;port=$port;dbname=pizzashop";
+    }
 
-        $mysqlCon = mysqli_connect(DB_HOST, DB_USER, DB_PASS, "", DB_PORT) or die("Error: " . mysqli_error($mysqlCon));
-        mysqli_select_db($mysqlCon, DB_NAME) or die("Error: " . mysqli_error($mysqlCon));
+    public static function getInstance() {
+        if (!isset(self::$instance)) {
+            $object = __CLASS__;
+            self::$instance = new $object;
+        }
+        return self::$instance;
     }
 
 }
 
-/* class DBConfig {
+/*
+class DBConfig {
 
     public static $DB_CONNSTRING = "mysql:host=localhost;dbname=pizzashop";
     public static $DB_USERNAME = "root";
