125184d4291ffbb98fef9887e6cb776e86d4cf4b
ValentinoGahide <valentinogahide@gmail.com>
2015-01-27 15:55:13 +0100

    test

--- data/DBConfig.php
+++ data/DBConfig.php
@@ -2,20 +2,19 @@
 
 namespace data;
 
-define('DB_HOST', getenv('OPENSHIFT_MYSQL_DB_HOST'));
-define('DB_PORT', getenv('OPENSHIFT_MYSQL_DB_PORT'));
-define('DB_USER', getenv('OPENSHIFT_MYSQL_DB_USERNAME'));
-define('DB_PASS', getenv('OPENSHIFT_MYSQL_DB_PASSWORD'));
-define('DB_NAME', getenv('OPENSHIFT_GEAR_NAME'));
-
 class DBConfig {
 
     private static $instance;
 
     function __construct() {
-        $dsn = 'mysql:dbname=' . DB_NAME . ';host=' . DB_HOST . ';port=' . DB_PORT;
-        $dbh = new PDO($dsn, DB_USER, DB_PASS);
-        return $dbh;
+        $DB_HOST = getenv('OPENSHIFT_MYSQL_DB_HOST');
+        $DB_PORT = getenv('OPENSHIFT_MYSQL_DB_PORT');
+        $DB_USER = getenv('OPENSHIFT_MYSQL_DB_USERNAME');
+        $DB_PASS = getenv('OPENSHIFT_MYSQL_DB_PASSWORD');
+        $DB_NAME = getenv('OPENSHIFT_GEAR_NAME');
+        
+        $dsn = 'mysql:dbname=' . $DB_NAME . ';host=' . $DB_HOST . ';port=' . $DB_PORT;
+        $dbh = new PDO($dsn, $DB_USER, $DB_PASS);
     }
 
     public static function getInstance() {
