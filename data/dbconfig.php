cdedaa04251a2d24af99498873c0c372d81ca9f9
ValentinoGahide <valentinogahide@gmail.com>
2015-01-27 09:08:27 +0100

    Openshift

--- data/dbconfig.php
+++ data/dbconfig.php
@@ -1,10 +1,23 @@
 <?php
+
 namespace Data;
 
-class DBConfig {
-    
-    public static $DB_CONNSTRING = "mysql:host=localhost;dbname=pizzashop";
-    public static $DB_USERNAME = "root";
-    public static $DB_PASSWORD = "";
-     
-} 
+if (!isset($DB)) {
+    $DB = new DBconfig();
+}
+
+class DBconfig {
+
+    public static $DB_CONNSTRING = null;
+    public static $DB_USERNAME = null;
+    public static $DB_PASSWORD = null;
+
+    function __construct() {
+        $host = getenv("OPENSHIFT_MYSQL_DB_HOST");
+        $port = getenv('OPENSHIFT_MYSQL_DB_PORT');
+        DBconfig::$DB_USERNAME = getenv('OPENSHIFT_MYSQL_DB_USERNAME');
+        DBconfig::$DB_PASSWORD = getenv('OPENSHIFT_MYSQL_DB_PASSWORD');
+        DBconfig::$DB_CONNSTRING = "mysql:host=$host;port=$port;dbname=databasename";
+    }
+
+}
