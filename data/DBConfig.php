98d11df6782938c94aa47ba5b18c664e83004f69
ValentinoGahide <valentinogahide@gmail.com>
2015-01-28 14:22:04 +0100

    test

--- data/DBConfig.php
+++ data/DBConfig.php
@@ -1,10 +1,25 @@
 <?php
-namespace Data;
+
+namespace data;
 
 class DBConfig {
-    
+
     public static $DB_CONNSTRING = "mysql:host=localhost;dbname=pizzashop";
     public static $DB_USERNAME = "root";
     public static $DB_PASSWORD = "";
-     
-} 
+    public $dbh; // handle of the db connexion
+    private static $instance;
+
+    private function __construct() {
+        $this->dbh = new PDO(self::$DB_CONNSTRING, self::$DB_USERNAME, self::$DB_PASSWORD);
+    }
+
+    public static function getInstance() {
+        if (!isset(self::$instance)) {
+            $object = __CLASS__;
+            self::$instance = new $object;
+        }
+        return self::$instance;
+    }
+
+}
--- data/plaatsDAO.php
+++ data/plaatsDAO.php
@@ -1,53 +1,52 @@
 <?php
-namespace Data;
+
+namespace data;
 
 use data\DBConfig;
 use PDO;
 use entities\Plaats;
 
-class plaatsDAO{
-    
+class plaatsDAO {
+
     //Haalt alle postcodes en plaatsen uit de database
     //output: $lijst: lijst van alle plaats-objecten
-    public function getAllePostcodePlaats(){
+    public function getAllePostcodePlaats() {
         $lijst = array();
-        
-        $dbh = new PDO(DBConfig::$DB_CONNSTRING, DBConfig::$DB_USERNAME, DBConfig::$DB_PASSWORD);
-        
+
+        $dbh = new PDO(self::$DB_CONNSTRING, self::$DB_USERNAME, self::$DB_PASSWORD);
+
         $sql = "SELECT plaatsId, postcode, plaats, leverplaats FROM plaats";
 
         $resultSet = $dbh->query($sql);
-        
+
         foreach ($resultSet as $rij) {
             $plaats = Plaats::create($rij["plaatsId"], $rij["postcode"], $rij["plaats"], $rij["leverplaats"]);
-            
+
             array_push($lijst, $plaats);
         }
 
         $dbh = null;
         return $lijst;
-        
     }
-    
+
     //Haalt een plaats op aan de hand van zijn id
     //input: $plaatsId: id van een plaats
     //output: $plaats: plaats-object van ingevoergde id
     //        null: id niet gevonden in de database
-    public function getPlaatsById($plaatsId){
-        $dbh = new PDO(DBConfig::$DB_CONNSTRING, DBConfig::$DB_USERNAME, DBConfig::$DB_PASSWORD);
-        
+    public function getPlaatsById($plaatsId) {
+        $dbh = new PDO(self::$DB_CONNSTRING, self::$DB_USERNAME, self::$DB_PASSWORD);
+
         $sql = "SELECT postcode, plaats, leverplaats FROM plaats WHERE plaatsId = $plaatsId";
 
         $resultSet = $dbh->query($sql);
         $rij = $resultSet->fetch();
-        if($rij > 0){
+        if ($rij > 0) {
             $plaats = Plaats::create($plaatsId, $rij["postcode"], $rij["plaats"], $rij["leverplaats"]);
             return $plaats;
-        }else{
+        } else {
             $dbh = null;
             return null;
         }
-        
     }
-    
+
 }
